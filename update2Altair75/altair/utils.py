bb30d7110b1314687f255459731939f038fc754e
Apua Juan <apua.juan@hp.com>
2015-01-22 17:35:25 +0800

    Provide "updating PublicKeys & Facility attr" tool

---
+++ update2Altair75/altair/utils.py
@@ -0,0 +1,36 @@
+def generate_uri(netloc='localhost', path='', Query={}):
+    """
+    issues:
+      - check if URI form is valid, refer RFC 3986
+
+    >>> generate_uri(path='/rest/os-deployment-jobs/', Query={'force': 'true'})
+    'https://localhost/rest/os-deployment-jobs/?force=true'
+    >>> generate_uri('https://altair.dev-net.local', '/rest/os-deployment-jobs/')
+    'https://altair.dev-net.local/rest/os-deployment-jobs/'
+    """
+    from urllib import quote
+    from urlparse import urljoin
+
+    if netloc.endswith('/'):
+        netloc = netloc[:-1]
+    if '://' in netloc:
+        scheme, netloc = netloc.split('://')
+    else:
+        scheme, netloc = 'https', netloc
+    if not path.startswith('/'):
+        path = '/' + path
+    query = '?'*bool(Query) + '&'.join(k+'='+quote(v) for k,v in Query.viewitems())
+    return "{scheme}://{netloc}{path}{query}".format(**locals())
+
+
+def _add_indent(json_content):
+    from json import dumps
+    return dumps(json_content, indent=2)
+
+
+def failure_information(response):
+    template = "\n\x1b[32mstatus code\x1b[m => \x1b[33m{}\x1b[m\n\x1b[34mcontent\x1b[m => {}"
+    return template.format(response.status_code, _add_indent(response.json()))
+
+
+
---
+++ update2Altair75/main.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+
+from ConfigParser import ConfigParser
+import sys
+
+from altair import Altair
+from colorprint import print, pprint
+
+if sys.version_info.major==2:
+    input = raw_input
+
+
+# Get attribute
+# =============
+
+CFG_FILE = 'settings.cfg'
+conf = ConfigParser()
+conf.read(CFG_FILE)
+
+current = dict(conf.items('Current Appliance'))
+appliance = dict(conf.items('Appliance'))
+
+
+# Retrieve data
+# =============
+
+with Altair(appliance_ip = current['ip'],
+            username = current['username'],
+            password = current['password']) as api:
+
+    print.reverse("-> Retrieve Data...")
+
+    # "Product Keys" and "Facility Custom Attributes"
+    facility_attr = api.retrieve_facility(1)['customAttributes']
+    current_altair_data = {}
+    current_altair_data['productkeys'] = {k:v
+        for k,v in facility_attr.iteritems()
+        if k.startswith('ProductKey_')}
+    current_altair_data['customattrs'] = {k:v
+        for k,v in facility_attr.iteritems()
+        if not k.startswith('ProductKey_') and
+           not k.startswith('__OPSW') and
+           k != 'device_discovery_naming_rules'}
+
+print.reverse("done\n")
+
+
+# Upload data
+# ===========
+
+with Altair(appliance_ip = appliance['ip'],
+            username = appliance['username'],
+            password = appliance['password']) as api:
+
+    print.reverse("-> Upload Data...")
+
+    facility = api.retrieve_facility(1)
+    facility['customAttributes'].update(current_altair_data['productkeys'])
+    facility['customAttributes'].update(current_altair_data['customattrs'])
+    api.edit_facility(1, facility)
+
+
+print.reverse("done\n")
+
+print.reverse("Task is Finished Successfully~")
+input("Press any key to Quit")
