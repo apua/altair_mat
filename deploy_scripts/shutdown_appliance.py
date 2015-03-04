2c7487b74fa7eef6ba71356ef091d6fed073d37a
Apua Juan <apua.juan@hp.com>
2015-03-04 11:09:21 +0800

    Initialized shutdown_appliance recipe

---
+++ deploy_scripts/shutdown_appliance.py
@@ -0,0 +1,21 @@
+import argparse
+
+from altair import Altair
+from tools import gen_path, get_config
+
+'''
+usage: $CMD $SETTINGS_FILENAME
+
+it can be implemented by turning off vm
+'''
+
+settings = gen_path('settings.cfg', __file__)
+config = get_config(settings)
+username = 'administrator'
+password = config[username]['password']
+
+with Altair(appliance_ip=appliance_ip,
+            username=username,
+            password=password) as api:
+
+    api.shutdown()
