a4df22e925983e2c981a1a3b3fb77f728ef9b46f
Apua Juan <apua.juan@hp.com>
2015-03-04 11:09:13 +0800

    Fixed bugs about writting and reading config

--- deploy_scripts/build_settings.py
+++ deploy_scripts/build_settings.py
@@ -1,46 +1,36 @@
 import argparse
 import sys
 
-#from altair import Altair
-from tools import gen_path, IP
+from altair import Altair
+from tools import gen_path, set_config, subconfig2value
 
 '''
 usage: $CMD $APPLIANCE_IP $SETTINGS_FILENAME
-
-content of settings.cfg::
-
-  [administrator]
-  password = 'hpvse123'
-  
-  [activation key]
-  [prodyct keys]
-  [facility attributes]
-  [network]
-  [media server]
-  username
-  password
-  
-  appliance_ip = ''
-  info_filename = ''
+ask administrator`s password
 '''
 
 settings = gen_path('settings.cfg', __file__)
-
+username = 'administrator'
 
 with Altair(appliance_ip=appliance_ip,
             username=username,
             password=password) as api:
 
-    info = api.get_info()
-    '''
-    info = {
-        'facility_attr': {
-            'ProductKey_*': '',
-            '__OPSW-Media-*: '',
-            ...,
-            },
-        'activation': ...,
-        # 'users': ...,
-        # 'osbps': ...,
+    config = {
+        'media settings': api.get_media_settings(),
+        'product keys': api.get_product_keys(),
+        'facility attributes': api.get_facility_attributes(),
+        'activation key': api.get_activation_key(),
+        'network settings': api.get_network_settings(),
         }
-    '''
+
+    users = api.get_users()
+    other_users = ...
+    login_user = ...
+    login_user['other users'] = subconfig2value(other_users)
+    config[username] = login_user
+
+    # OSBPs
+    # WinPE
+
+set_config(settings, config)
---
+++ deploy_scripts/set_appliance.py
@@ -0,0 +1,30 @@
+import argparse
+
+from altair import Altair
+from tools import gen_path, get_config, value2subconfig
+
+'''
+usage: $CMD $SETTINGS_FILENAME
+'''
+
+settings = gen_path('settings.cfg', __file__)
+config = get_config(settings)
+username = 'administrator'
+password = config[username]['password']
+
+with Altair(appliance_ip=appliance_ip,
+            username=username,
+            password=password) as api:
+
+    api.set_media_settings(config['media settings'])
+    api.set_product_keys(config['product keys'])
+    api.set_facility_attributes(config['facility attributes'])
+    api.set_activation_key(config['activation key'])
+
+    other_users = config[username]['other users']
+    for section, items in value2subconfig(other_users):
+        items['username'] = section
+        api.add_user(items)
+
+    # OSBPs
+    # WinPE
