e7620ffed4ba59125c4d76f5feb4b931383e0ce4
Apua Juan <apua.juan@hp.com>
2015-03-05 10:09:37 +0800

    Impletmented get/set facility custom attributes

--- deploy_scripts/altair/__init__.py
+++ deploy_scripts/altair/__init__.py
@@ -1,38 +1,98 @@
+from __future__ import print_function
+
+import requests
+
+
 class Altair(object):
 
-    # media settings, product keys, facility attributes
-    # =================================================
+    def __init__(self, appliance_ip, username, password, trust_env=False):
+
+        # build HTTP connection session
+        self.conn = requests.Session()
+        self.conn.trust_env = trust_env
+        self.conn.verify = False
+
+        # set target appliance and authenticate
+        self.appliance_ip = appliance_ip
+        self.username = username
+        self.session_id = self._authenticate(
+             username=username,
+             password=password,
+             )['sessionID']
+
+    def __enter__(self):
+        '''
+        test if session is outdated
+        '''
+        self._retrieve_session()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._remove_session()
+
+    # media settings, product keys, facility attributes, pxeboot default
+    # ==================================================================
 
     def get_media_settings(self):
-        pass
-
-    def set_media_settings(self, settings):
-        pass
+        customAttributes = self._retrieve_facility(1)['customAttributes']
+        media_settings = {k: v for k, v in customAttributes.items()
+                               if k.startswith('__OPSW-Media')}
+        media_settings['__OPSW-Media-WinPassword'] = ''
+        return media_settings
 
     def get_product_keys(self):
-        pass
-
-    def set_product_keys(self, keys):
-        pass
+        customAttributes = self._retrieve_facility(1)['customAttributes']
+        product_keys = {k: v for k, v in customAttributes.items()
+                             if k.startswith('ProductKey_')}
+        return product_keys
 
     def get_facility_attributes(self):
-        pass
+        customAttributes = self._retrieve_facility(1)['customAttributes']
+        product_keys = {k: v for k, v in customAttributes.items()
+                             if not k.startswith('ProductKey_') and
+                                not k.startswith('__OPSW') and
+                                k != 'device_discovery_naming_rules'}
+        return product_keys
 
-    def set_facility_attributes(self, attrs):
-        pass
+    def get_pxeboot_default(self):
+        customAttributes = self._retrieve_facility(1)['customAttributes']
+        attr_name = '__OPSWpxeboot_default'
+        return {attr_name: customAttributes[attr_name]}
+
+    def set_media_settings(self, settings):
+        facility = self._retrieve_facility(1)
+        facility['customAttributes'].update(settings)
+        self._edit_facility(1, facility)
+
+    def set_product_keys(self, keys):
+        facility = self._retrieve_facility(1)
+        facility['customAttributes'].update(keys)
+        self._edit_facility(1, facility)
+
+    def set_facility_attributes(self, attributes):
+        facility = self._retrieve_facility(1)
+        facility['customAttributes'].update(attributes)
+        self._edit_facility(1, facility)
+
+    def set_pxeboot_default(self, default):
+        facility = self._retrieve_facility(1)
+        facility['customAttributes'].update(default)
+        self._edit_facility(1, facility)
 
     # the activation key
     # ==================
 
     def get_activation_key(self):
         from base64 import b64encode
-        key = ''
+        key = self._retrieve_activation()['activationCode']
+        print(key)
         return b64encode(key)
 
     def set_activation_key(self, key_):
         from base64 import b64decode
-        key = b64decode(key_)
-        pass
+        activation = self._retrieve_activation()
+        activation['activationCode'] =  b64decode(key_)
+        self._send_activation(activation)
 
     # OSBPs and related
     # =================
@@ -62,7 +122,9 @@ class Altair(object):
         pass
 
     def change_password(self, new_password, old_password):
-        pass
+        self._update_user({'userName': self.username,
+                           'password': new_password,
+                           'currentPassword': old_password})
 
     # network
     # =======
@@ -78,15 +140,17 @@ def collect_methods(*module_names):
     from importlib import import_module
 
     for module_name in module_names:
-        module = import_module(module_names, __path__)
+        module = import_module(module_name, __path__)
         for attr_name in dir(module):
             if not attr_name.startswith('_'):
                 attr_value = getattr(module, attr_name)
                 yield attr_name, attr_value
 
 
-for name, method in collect_methods():
+for name, method in collect_methods('rest_api'):
     setattr(Altair, '_'+name, method)
 
 for name, method in collect_methods():
     setattr(Altair, name, method)
+
+requests.packages.urllib3.disable_warnings()
