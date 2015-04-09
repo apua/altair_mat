4407ea65290d31fc8b3739831ac8bcf66f0e9c44
Apua Juan <apua.juan@hp.com>
2015-04-09 10:42:43 +0800

    Remove get_osbps.py

--- customized_OSBP/get_osbps.py
+++
@@ -1,9 +0,0 @@
-from altair import Altair
-from pprint import pprint as p
-import json
-
-with Altair(appliance_ip='10.30.1.235', username='administrator', password='Compaq123') as api:
-
-    j = api._list_OSBP()
-    with open('osbps.json','w') as fp:
-        json.dump(j, fp, indent=2)
