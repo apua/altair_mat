ad43b3c7adf8e2d404e8fa6e71b09ea66080309b
Apua Juan <apua.juan@hp.com>
2015-04-09 10:21:19 +0800

    Renames

--- customized_OSBP/get_osbps.py
+++ customized_OSBP/get_osbps.py
@@ -4,8 +4,6 @@ import json
 
 with Altair(appliance_ip='10.30.1.235', username='administrator', password='Compaq123') as api:
 
-    L = api._list_OSBP()
-
-    p(L)
-    with open('osbp.json','w') as f:
-        json.dump(L, f)
+    j = api._list_OSBP()
+    with open('osbps.json','w') as fp:
+        json.dump(j, fp, indent=2)
