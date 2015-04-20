5f32336fcbe8a496fa7a82052c34755270614087
Apua Juan <apua.juan@hp.com>
2015-04-20 19:27:07 +0800

    Finish download_latest_altair.py

--- deploy_scripts/download_latest_altair.py
+++ deploy_scripts/download_latest_altair.py
@@ -28,49 +28,47 @@ def get_latest_filename():
     return latest_filename
 
 
-def download_file(filename):
-    path = os.path
-    stdout = sys.stdout
+def write_file(stream, f, total):
+    form = '\r{percent:6.2f}% size: {part} / {total}'
+    show = lambda s: sys.stdout.write(s) or sys.stdout.flush()
+    output = lambda **k: show(form.format(**k))
+    fwrite = lambda d: f.write(d) or f.flush()
 
-    filepath = path.join(LOCATION, filename)
-    assert not path.exists(filepath), (
-        '"{}" already exists\n'.format(filepath)
-        )
+    for i, chunk in enumerate(stream):
+        fwrite(chunk)
+        part = i*BUFFER_SIZE
+        output(percent=part/total*100, part=part, total=total)
+    else:
+        output(percent=100, part=total, total=total)
 
-    fileuri = urlparse.urljoin(NIGHTLY_BUILD_PAGE, filename)
+
+def download_file(fileuri, filepath):
     resp = requests.get(fileuri, stream=True)
     stream = resp.iter_content(chunk_size=BUFFER_SIZE)
-    content_length = resp.headers['content-length']
-
-    total = int(content_length)
-    output_form = '\r{percent:6.2f} size: {now} / {total}'
+    filesize = int(resp.headers['content-length'])
 
     diskspace = get_diskspace()
-    assert total < diskspace, (
-        'File size:  {}\n'
-        'Disk space: {}\n'
-        'Disk space is not enough\n'.format(total, diskspace)
-        )
+    if not filesize < diskspace:
+        print('File size:  {}\n'
+              'Disk space: {}\n'
+              'Disk space is not enough'.format(filesize, diskspace))
+        exit(1)
 
-    with open(filepath, 'wb') as f:
-        for i, chunk in enumerate(stream):
-            if chunk:
-                f.write(chunk)
-                f.flush()
-                now = i*BUFFER_SIZE
-                percent = now / total * 100
-                stdout.write(output_form.format(**locals()))
-                stdout.flush()
-        percent, now = 100, total
-        stdout.write(output_form.format(**locals()) + '\n')
-        stdout.flush()
+    with open(filepath, 'wb') as fd:
+        try:
+            write_file(stream, fd, filesize)
+        except KeyboardInterrupt:
+            os.remove(filepath)
+        finally:
+            sys.stdout.write('\n')
 
 
 if __name__=='__main__':
     filename = get_latest_filename()
-    try:
-        download_file(filename)
-    except KeyboardInterrupt:
-        os.remove(filename)
-    except AssertionError as E:
-        sys.stdout.write(E.message)
+    filepath = os.path.join(LOCATION, filename)
+    fileuri = urlparse.urljoin(NIGHTLY_BUILD_PAGE, filename)
+
+    if os.path.exists(filepath):
+        '"{}" already exists\n'.format(filepath)
+
+    download_file(fileuri, filepath)
