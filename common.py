6f148bc6c51bae2d64a9f059d0e895094bd66e23
Apua Juan <apua.juan@hp.com>
2015-04-20 17:18:25 +0800

    Update common script

--- common.py
+++ common.py
@@ -1,31 +1,23 @@
-# for Python shell
-try:
-    import readline
-    import rlcompleter
-    import atexit
-    import os
-except ImportError:
-    print("Python shell enhancement modules not available.")
-else:
-    histfile = os.path.join(os.environ["HOME"], ".pythonhistory")
-    import rlcompleter
-    readline.parse_and_bind("tab: complete")
-    if os.path.isfile(histfile):
-        readline.read_history_file(histfile)
-    atexit.register(readline.write_history_file, histfile)
-    del os, histfile, readline, rlcompleter, atexit
-    print("Python shell history and tab completion are enabled.")
-
-
-# altair
+# libs
 from altair import Altair
 
-
-# yaml
 import yaml
 from utils import set_config, get_config
 
-# sys
+from pprint import pprint
+
+import os
 import sys
 
+
+# shell
+execfile(os.path.expanduser('~/.pythonstartup.py'))
+
+
+# constants
 CONFIG_FILENAME = 'settings.cfg'
+
+
+# methods
+altair235 = lambda: Altair(appliance_ip='10.30.1.235', username='administrator', password='Compaq123')
+p = lambda v: pprint(v, depth=1)
