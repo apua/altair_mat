07e26ff5b82235ae2af986d1a9a9bd5a08c57ce2
Apua Juan <apua.juan@hp.com>
2015-04-21 15:12:19 +0800

    Updated common

--- common.py
+++ common.py
@@ -1,18 +1,25 @@
-# libs
 from __future__ import division, print_function, absolute_import
 
+# shell
+import os
+execfile(os.path.expanduser('~/.pythonstartup.py'))
+
+
+# libs
+from getpass import getpass
 from pprint import pprint
 import os
+import subprocess
 import sys
 
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
 import requests
 import yaml
 
 from altair import Altair
 from utils import *
-
-# shell
-execfile(os.path.expanduser('~/.pythonstartup.py'))
 
 
 # constants
