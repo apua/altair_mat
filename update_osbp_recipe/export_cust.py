936a80bf50dd8d7c274dd9a8948f37c5f16b3eaa
Apua Juan <apua.juan@hp.com>
2015-05-27 22:37:00 +0800

    Remove unnecessary variables and import statements

--- update_osbp_recipe/export_cust.py
+++ update_osbp_recipe/export_cust.py
@@ -1,28 +1,17 @@
-from pprint import pprint as p
-import json
-import os
-import sys
-import time
-
-sys.path.append('../common/')
-
+__import__('sys').path.append('../common/')
 
 from altair import Altair
 from utils import get_config, set_config
 
-
 settings = get_config('settings.txt')
-
 appliance_ip  = settings['appliance_ip']
 username      = settings['username']
 password      = settings['password']
 cust_filepath = settings['cust_filepath']
 
-
 with Altair(appliance_ip, username, password) as api:
     cust_info = api.get_cust_info()
     #cust_info = api.get_cust_info(interval=1)
     set_config(cust_info, cust_filepath)
-
 
 raw_input('Press any key to continue...')
