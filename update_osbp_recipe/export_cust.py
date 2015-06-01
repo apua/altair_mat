70714c26872902f81e27b2ab1ae41812cb2b72f5
Apua Juan <apua.juan@hp.com>
2015-06-01 10:22:56 +0800

    Add file replacing check to export script
    Change-Id: I5c3a65e4ccc3748604d636b245d4e6cf121e7e02

--- update_osbp_recipe/export_cust.py
+++ update_osbp_recipe/export_cust.py
@@ -9,6 +9,16 @@ username      = settings['username']
 password      = settings['password']
 cust_filepath = settings['cust_filepath']
 
+try:
+    get_config(cust_filepath)
+    mesg = '"{}" already exists, replace it? (Y/N) '.format(cust_filepath)
+    if raw_input(mesg).lower()[0]!='y':
+        print('Stop to export customized OSBPs')
+        raw_input('Press any key to continue...')
+        exit()
+except IOError:
+    pass
+
 with Altair(appliance_ip, username, password) as api:
     cust_info = api.export_cust_info()
     #cust_info = api.export_cust_info(interval=1)
