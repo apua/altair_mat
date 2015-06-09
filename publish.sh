ad8616429dbd4d6df86cab03370c4678bfe5ff48
Apua Juan <apua.juan@hp.com>
2015-06-09 18:46:52 +0800

    Fix a bug of publish script
    Change-Id: I87fec08dc1b78bb7b7d59adae17be944cc62570d

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@ version=0.1
 tmp=/tmp
 filename=${repo}-${version}.zip
 
-cd ${repo}
-git archive --prefix=${repo} --format zip master -o ${tmp}/${filename}
+cd ~/${repo}
+git archive --prefix=${repo}/ --format zip master -o ${tmp}/${filename}
 
 cd ${tmp}
 smbclient //csinfs.americas.hpqcorp.net/pub -U csi%csi -c "cd ${repo} ; put ${filename}"
