7acecd0a3799345aba373806a62dde2d3326d9b3
Apua Juan <apua.juan@hp.com>
2015-06-09 14:34:51 +0800

    Add publishing script
    Change-Id: I8a227408eff239209526c38efd6f46dd8b2a9621

---
+++ publish.sh
@@ -0,0 +1,10 @@
+repo=altair-recipe
+version=0.1
+tmp=/tmp
+filename=${repo}-${version}.zip
+
+cd ${repo}
+git archive --prefix=${repo} --format zip master -o ${tmp}/${filename}
+
+cd ${tmp}
+smbclient //csinfs.americas.hpqcorp.net/pub -U csi%csi -c "cd ${repo} ; put ${filename}"
