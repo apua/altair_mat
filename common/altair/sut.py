eddde9ae859101f01218dce90066b9d655aa8d87
Apua Juan <apua.juan@hp.com>
2015-06-24 15:15:51 +0800

    Implement get suts feature
    Change-Id: I31f891a28a923e0fe6879fbab3920bd1381bb39a

--- common/altair/sut.py
+++ common/altair/sut.py
@@ -1,5 +1,19 @@
 def get_suts(api):
-    pass
+    def get_attr(sut):
+        cust_attrs = {}
+        for d in sut['customAttributes']:
+            name, scope, value = d['key'], d['values'][0]['scope'], d['values'][0]['value']
+            if scope!='facility' and not name.startswith('__OPSW'):
+                cust_attrs[name] = value
+        return cust_attrs
+
+    suts = (api._retrieve_server(uri=m['uri'])
+            for m in api._list_index({'category': 'osdserver'})['members'])
+    return [{'ilo_ip_address': sut['ilo']['ipAddress'],
+             'username': sut['ilo']['username'],
+             'password': '....',
+             'custom_attributes': get_attr(sut)}
+            for sut in suts]
 
 def add_sut(api):
     pass
