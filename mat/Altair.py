3ae2cbd83275f02422904be23871b47d1c671e67
Apua Juan <apua.juan@hp.com>
2015-06-25 10:33:03 +0800

    Add own Altair keyword library
    Change-Id: I6f1a478695d30fafdda6aa03169f61d47088abe7

---
+++ mat/Altair.py
@@ -0,0 +1,34 @@
+r"""
+It is a Robot Framework Library which provides keywords to control Altair with API
+"""
+
+__import__('sys').path.append('../common/')
+
+from inspect import getmembers
+from robot.api import logger
+from altair.api import Altair as API
+
+class Altair(object):
+    r"""
+    A hybrid library class
+    """
+    ROBOT_LIBRARY_VERSION = __version__ = '0.142857'
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+
+    def set_appliance_ip(self, appliance_ip):
+        self.api = API(appliance_ip)
+        return self.api
+
+
+def gen_unbound_method(name):
+    unbound_method = lambda self, *a, **k: getattr(self.api, name)(*a, **k)
+    unbound_method.__name__ = name
+    return unbound_method
+
+
+for name, value in getmembers(API):
+    if name.startswith('_'):
+        continue
+    keyword = name
+    unbound_method = gen_unbound_method(name)
+    setattr(Altair, keyword, unbound_method)
